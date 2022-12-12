d410f8cb46d26259c3768feb6cd7000e3a91881e
erenkara1907 <erenkaraaa47@gmail.com>
2022-12-12 09:06:42 +0300

    Initial Commit

---
+++ DaPay/DaPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaPayApp.swift
+//  DaPay
+//
+//  Created by Eren Kara on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DaPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
